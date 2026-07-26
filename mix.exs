dfe63810b29eef4c1682a4bdb4a0cbcb810abf6c
Adrian Irving-Beer <wisq@wisq.net>
2026-07-25 23:25:50 -0400

    Add dependencies.

--- mix.exs
+++ mix.exs
@@ -23,6 +23,10 @@ defmodule TrieBenchmark.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:benchee, ">= 0.0.0"},
+      {:trie_hard, "~> 0.2"},
+      {:retrieval, "~> 0.9"},
+      {:trie, git: "https://github.com/dimitarvp/trie", tag: "master"}
     ]
   end
 end
