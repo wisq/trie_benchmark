30f1567a9850f91c92556a69769bc2c173c01664
Adrian Irving-Beer <wisq@wisq.net>
2026-07-25 23:40:04 -0400

    squash deps

--- mix.exs
+++ mix.exs
@@ -21,8 +21,6 @@ defmodule TrieBenchmark.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:benchee, ">= 0.0.0"},
       {:trie_hard, "~> 0.2"},
       {:retrieval, "~> 0.9"},
