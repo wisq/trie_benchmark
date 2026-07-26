90151b979b97fa69bbbe2f2673762b1fea69a5d8
Adrian Irving-Beer <wisq@wisq.net>
2026-07-25 23:21:37 -0400

    mix new trie_benchmark

---
+++ lib/trie_benchmark.ex
@@ -0,0 +1,18 @@
+defmodule TrieBenchmark do
+  @moduledoc """
+  Documentation for `TrieBenchmark`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TrieBenchmark.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/trie_benchmark_test.exs
@@ -0,0 +1,8 @@
+defmodule TrieBenchmarkTest do
+  use ExUnit.Case
+  doctest TrieBenchmark
+
+  test "greets the world" do
+    assert TrieBenchmark.hello() == :world
+  end
+end
